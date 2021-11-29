3b753e1afba917748c78d9fe057d5c3dc27fec58
Chris Williams <cw.chris.williams@gmail.com>
2021-11-29 22:02:04 +0000

    Add documentation

--- src/flickr_fetcher/flickr_fetcher.clj
+++ src/flickr_fetcher/flickr_fetcher.clj
@@ -10,7 +10,8 @@
 (def default-port 80)
 (def default-app-settings-path "./appsettings.json")
 
-(def exit-codes {:unexpected-error -1 :ok 0 :failed-to-parse-args 1 :failed-to-load-config 2})
+(def exit-codes "Map of exception type to exit code"
+  {:unexpected-error -1 :ok 0 :failed-to-parse-args 1 :failed-to-load-config 2})
 
 (def cli-options
   "A definition of CLI options as defined in clojure.tools.cli"
@@ -47,22 +48,44 @@
       {:options options :summary summary})))
 
 (defn process-cli-args
+  "Processes the CLI options with the standard cli-parser
+  Arguments:
+    - A list of arguments as defined in the documentation
+    - A cli-options as defined in clojure.tools.cli
+  Returns:
+    - Map containing :exit-message (opt) - string to display to the user on the cli
+                     :exit-status (opt) - error exit status if an error occurred
+                     :options (opt) - The options provided on the command line"
   [args cli-options]
   (process-cli-args-with-cli-options-and-parser args cli-options cli/parse-opts))
 
-
 (defn start-service
+  "Starts the service, loding the config and starting the server
+  Arguments:
+    - An options containing the keys :config and :port"
   [options]
   (config/load-config (:config options))
   (svr-mgmnt/start-server (:port options)))
 
 (defn get-exit-code-from-exception
+  "Gets the exit code for the exception provided
+  Arguments:
+    - An exception
+  Returns:
+    - An exit code, or -1 if the exception was unexpected"
   [ex]
   (let [data (or (ex-data ex) {})
         ex-type (get data :type :unexpected-error)]
     (get exit-codes ex-type -1)))
 
 (defn parse-args-and-start-service-with-fn-map
+  "Evaluates the arguments and configuration file and starts the server based on provided args
+
+  Arguments:
+    - A hash map with the keys :start-service-fn :print-fn :log-fn
+    - args as strings as defined in documentation
+  Returns:
+    - An exit code to be returned to the user"
   [{:keys [start-service-fn print-fn log-fn]} args]
   (try
     (let [{:keys [summary options]} (process-cli-args args cli-options)]
